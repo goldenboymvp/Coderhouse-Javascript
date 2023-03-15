975a6149a17e82207d5e0a6923262c3c955c57e2
DESKTOP-EVBFRI3\ezequ <ezequiel.jerez@hotmail.com>
2023-03-14 23:31:33 -0300

    entrega final js

--- javascript/cart.js
+++ javascript/cart.js
@@ -3,130 +3,144 @@ shoppingCart = [];
 
 const productContainer = document.getElementById("product-container");
 const cartContainer = document.getElementById("cartItems");
-const totalPrice = document.getElementById("total-price")
+const totalPrice = document.getElementById("total-price");
+const checkoutButton = document.getElementById("btn-checkout");
+const itemRow = document.getElementById("item");
+
 
 
 // Funciones
+// Sweetalert
+// Sweetalert Checkout
+function triggerSweetAlertCheckout(title, message, alertType) {
+    Swal.fire({
+        title: "Pedido procesado",
+        text: "El pedido fue enviado a Cocina",
+        icon: "success",
+        confirmButtonText: 'OK',
+    })
+};
 
-
-function totalSum(burger) {
-    let total = burger.reduce(function (acc, burger) { return acc + burger.price; }, 0);
-    saveStorage(total)
+function toastifyAdd(text) {
+    Toastify({
+        text: "Agregado al carrito",
+        duration: 3500,
+        close: true,
+        gravity: "bottom", 
+        position: "right", 
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "linear-gradient(to right, #343a40, #fd7e14)",
+        },
+        onClick: function(){} // Callback after click
+        }).showToast();
+}
+// Total
+function totalSum(burgers) {
+    let total = 0
+    console.log(burgers);
+    const forEachCallBack = (burger) => {
+        console.log(burger);
+        total = total + burger.price
+    };
+    burgers.forEach(forEachCallBack)
+    console.log(total);
     return total
 }
 
+
+// Storage
 function saveStorage(total) {
     sessionStorage.setItem("total", JSON.stringify(total));
 }
 
-function getProductsJSON() {
-    return fetch('products.json')
-            .then(products => {
-                products.json();})
-        .then((data) => {return data;})
-}
 
-// remove 
-function removeBurger(id) {
-    shoppingCart = shoppingCart.filter((burger) => {
-        return burger.id !== id;
-    })
-    const renderedCartProducts = shoppingCart.map((product) => {
-        const { id, image, description, title, price } = product;
-        return `<div class="cart-items">
-        <div class="item">
-            <div class="item-name">${title}</div>
-        <div class="item-price">$${price}</div>
-            <button class="remove-item" id="${id}">Remove</button>
-    </div>
-    $${totalPrice}`
-    })
-    cartContainer.innerHTML = renderedCartProducts.join('');
-    totalPrice.innerHTML = `$ ${totalSum(shoppingCart)}`;
-};
-
-function triggerSweetAlert(title, message, alertType) {
-    Swal.fire({
-        title: "Burger Agregada!",
-        text: "El item se agrego al carrito",
-        icon: "success",
-        confirmButtonText: 'OK',
-        buttons: {
-            confirm: {
-                text: "Ver Carrito",
-                value: true,
-                visible: true,
-                className: "btn btn-primary",
-                closeModal: true
-            },
-            cancel: {
-                text: "Seguir Comprando",
-                value: false,
-                visible: true,
-                className: "btn btn-secondary",
-                closeModal: true,
-            }
-        }
-    }).then((value) => {
-        if (value) {
-            // redirigirse
-            window.location.href = "#";
-        } else {
-            // quedarse
-        }
-    });
-}
-
-debugger;
-const products = []
-
-// map
-
-const renderedProducts = products.map((product) => {
-    const { id, image, description, title, price } = product;
-    return `<div class="col d-flex">
-    <div class="card shadow" style="width: 20rem;" alt="burguer">
-        <img class="card-img-top mx-auto d-block" src="/${image}">
-        <div class="card-body text-center">
-            <h5 class="card-title">${title}</h5>
-                <p class="card-text">${description}</p>
-                <div class="price-quantity">
-                    <h2>$ ${price}</h2>
-                </div>
-                <a href="#" class="btn-cart" id="${id}">Agregar al pedido</a>
-        </div>
-    </div>
-    </div>`
-})
-
-
-productContainer.innerHTML = renderedProducts.join('')
-
-let buttonAddToCart = document.querySelectorAll(".btn-cart");
-// add al carrito
-buttonAddToCart.forEach(button => {
-    button.addEventListener("click", () => {
-        const product = products.find((product) => {
-            return (product.id === button.id)
+// JSON FETCH
+async function getProductsJSON() {
+    return await fetch('./products.json')
+        .then(products => {
+            return products.json();
         })
-        shoppingCart.push(product);
-        const renderedCartProducts = shoppingCart.map((product) => {
-            const { id, image, description, title, price } = product;
-            return `<div class="cart-items">
-                <div class="item">
+        .then((data) => { return data; })
+}
+
+
+// Populate
+async function populateProducts() {
+    const products = await getProductsJSON()
+    console.log({ products });
+    const renderedProducts = products.map((product) => {
+        const { id, image, description, title, price } = product;
+        return `<div class="col d-flex">
+        <div class="card shadow" style="width: 20rem;" alt="burguer">
+            <img class="card-img-top mx-auto d-block" src="/${image}">
+            <div class="card-body text-center">
+                <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${description}</p>
+                    <div class="price-quantity">
+                        <h2>$ ${price}</h2>
+                    </div>
+                    <button class="btn-cart" id="${id}">Agregar al pedido</button>
+            </div>
+        </div>
+        </div>`
+    })
+
+    productContainer.innerHTML = renderedProducts.join('')
+
+    let buttonAddToCart = document.querySelectorAll(".btn-cart");
+    buttonAddToCart.forEach(button => {
+        button.addEventListener("click", () => {
+            const product = products.find((product) => {
+                return (product.id === button.id)
+            })
+            shoppingCart.push(product);
+            const renderedCartProducts = shoppingCart.map((product) => {
+                const { id, image, description, title, price } = product;
+                return `<div class="item">
                     <div class="item-name">${title}</div>
                 <div class="item-price">$${price}</div>
                     <button class="remove-item" id="${id}">Remove</button>
-            </div>
-            <div id="total-price">$${totalPrice}</div>`
-        })
-        cartContainer.innerHTML = renderedCartProducts.join('');
-        totalPrice.innerHTML = `$ ${totalSum(shoppingCart)}`;
-        triggerSweetAlert();
-    });
-})
+            </div>`
+            })
+            cartContainer.innerHTML = renderedCartProducts.join('');
+            const total = totalSum(shoppingCart);
+            totalPrice.innerHTML = `<p>Total  $${total}</p>`;
+            toastifyAdd();
+        });
+    })
+}
 
 
-// <div><img class="card-img-top img-thumbnail" src="/${image}">  ${title}  $${price}</div>
-//   <button class="btn btn-remove-cart" type="button" onclick="removeBurger('${id}')">Remover</button>
+// remove no lo puedo hacer funcionar
+
+function removeBurger() {
+    button.addEventListener
+    // const indexToRemove = shoppingCart.findIndex((burger) => burger.id === id);
+    // if (indexToRemove !== -1) {
+    //     shoppingCart.splice(indexToRemove, 1);
+    // }
+    // const renderedCartProducts = shoppingCart.map((product) => {
+    //     const { id, image, description, title, price } = product;
+    //     return `<div class="item">
+    //         <div class="item-name">${title}</div>
+    //         <div class="item-price">$${price}</div>
+    //         <button class="remove-item" id="${id}" onclick="removeBurger(${id})">Remove</button>
+    //     </div>`
+    // })
+    // cartContainer.innerHTML = renderedCartProducts.join('');
+    // totalPrice.innerHTML = `<span>$ ${totalSum(shoppingCart)}</span>`;
+};
+
+checkoutButton.addEventListener("click", checkout); // lo mismo con el checkout, este vacio o lleno el shoppingcart me tira el sweetalert D:
+function checkout() {
+    const cartItems = getProductsJSON();
+    if (cartItems.length === 0) {
+        alert("sarasa")
+    } else { triggerSweetAlertCheckout(); }
+}
+
+
+populateProducts();
+
 
